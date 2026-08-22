5293f02d4f20e447364d5a13266a879e154a72f6
Sumer Thakur <sumerthakur0306@gmail.com>
2026-08-22 17:06:01 +0530

    Time: 115 ms (72.86%), Space: 29 MB (74.93%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.py
@@ -0,0 +1,49 @@
+import heapq
+class Solution:
+    def totalCost(self, costs: List[int], k: int, candidates: int) -> int:
+        left_heap=[]
+        right_heap=[]
+        left=0
+        right=len(costs)-1
+
+        while left <= right and len(left_heap) < candidates:
+            heapq.heappush(left_heap,costs[left])
+            left+=1
+        while left <= right and len(right_heap) < candidates:
+            heapq.heappush(right_heap,costs[right])
+            right-=1        
+        total=0
+        for _ in range(k):
+            if not left_heap:
+                total+=heapq.heappop(right_heap)
+
+                if left<=right:
+                    heapq.heappush(right_heap,costs[right])
+                    right-=1  
+
+            elif not right_heap:
+                total+=heapq.heappop(left_heap)      
+
+                if left<=right:
+                    heapq.heappush(left_heap,costs[left])
+                    left+=1
+
+            elif left_heap[0]<=right_heap[0]:
+                total+=heapq.heappop(left_heap)
+
+                if left<=right:
+                    heapq.heappush(left_heap,costs[left])
+                    left+=1
+
+            else:    
+                total+=heapq.heappop(right_heap)      
+
+                if left<=right:
+                    heapq.heappush(right_heap,costs[right])
+                    right-=1
+
+        return total            
+    
+
+
+
